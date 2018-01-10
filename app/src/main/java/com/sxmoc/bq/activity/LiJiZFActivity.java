fc079a882846607471e60acf31646914ebd32676
ZhangJieBo <530092772@qq.com>
2018-01-10 11:50:21 +0800

    立即支付

---
+++ app/src/main/java/com/sxmoc/bq/activity/LiJiZFActivity.java
@@ -0,0 +1,59 @@
+package com.sxmoc.bq.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sxmoc.bq.R;
+import com.sxmoc.bq.base.ZjbBaseActivity;
+
+public class LiJiZFActivity extends ZjbBaseActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_li_ji_zf);
+        init();
+    }
+
+    @Override
+    protected void initSP() {
+
+    }
+
+    @Override
+    protected void initIntent() {
+
+    }
+
+    @Override
+    protected void findID() {
+
+    }
+
+    @Override
+    protected void initViews() {
+        ((TextView) findViewById(R.id.textViewTitle)).setText("立即支付");
+    }
+
+    @Override
+    protected void setListeners() {
+        findViewById(R.id.imageBack).setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageBack:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+}
