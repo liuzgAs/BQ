9c45e56d8bae6236f92704c55b8505be78e51f7f
ZhangJieBo <530092772@qq.com>
2018-01-11 14:24:47 +0800

    我的界面

--- app/src/main/java/com/sxmoc/bq/fragment/WoDeFragment.java
+++ app/src/main/java/com/sxmoc/bq/fragment/WoDeFragment.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.sxmoc.bq.R;
+import com.sxmoc.bq.base.ZjbBaseFragment;
+import com.sxmoc.bq.util.ScreenUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WoDeFragment extends Fragment {
+public class WoDeFragment extends ZjbBaseFragment {
 
+
+    private View mInflate;
+    private View viewBar;
 
     public WoDeFragment() {
         // Required empty public constructor
@@ -24,7 +29,45 @@ public class WoDeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wo_de, container, false);
+        if (mInflate == null) {
+            mInflate = inflater.inflate(R.layout.fragment_wo_de, container, false);
+            init();
+        }
+        //缓存的rootView需要判断是否已经被加过parent， 如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。
+        ViewGroup parent = (ViewGroup) mInflate.getParent();
+        if (parent != null) {
+            parent.removeView(mInflate);
+        }
+        return mInflate;
     }
 
+    @Override
+    protected void initIntent() {
+
+    }
+
+    @Override
+    protected void initSP() {
+
+    }
+
+    @Override
+    protected void findID() {
+        viewBar = mInflate.findViewById(R.id.viewBar);
+    }
+
+    @Override
+    protected void initViews() {
+        viewBar.setPadding(0, ScreenUtils.getStatusBarHeight(getActivity()),0,0);
+    }
+
+    @Override
+    protected void setListeners() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
