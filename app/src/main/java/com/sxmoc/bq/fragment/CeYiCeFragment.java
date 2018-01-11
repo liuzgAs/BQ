4eed9dcdbff7a20d788f828d3c1ec9b621056f87
ZhangJieBo <530092772@qq.com>
2018-01-11 14:58:36 +0800

    2018年1月11日14:58:29

--- app/src/main/java/com/sxmoc/bq/fragment/CeYiCeFragment.java
+++ app/src/main/java/com/sxmoc/bq/fragment/CeYiCeFragment.java
@@ -8,12 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.sxmoc.bq.R;
+import com.sxmoc.bq.base.ZjbBaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CeYiCeFragment extends Fragment {
+public class CeYiCeFragment extends ZjbBaseFragment implements View.OnClickListener {
 
+
+    private View mInflate;
 
     public CeYiCeFragment() {
         // Required empty public constructor
@@ -24,7 +27,56 @@ public class CeYiCeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ce_yi_ce, container, false);
+        if (mInflate == null) {
+            mInflate = inflater.inflate(R.layout.fragment_ce_yi_ce, container, false);
+            init();
+        }
+        //缓存的rootView需要判断是否已经被加过parent， 如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。
+        ViewGroup parent = (ViewGroup) mInflate.getParent();
+        if (parent != null) {
+            parent.removeView(mInflate);
+        }
+        return mInflate;
     }
 
+    @Override
+    protected void initIntent() {
+
+    }
+
+    @Override
+    protected void initSP() {
+
+    }
+
+    @Override
+    protected void findID() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setListeners() {
+        mInflate.findViewById(R.id.btnKaiShiJC).setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnKaiShiJC:
+
+                break;
+            default:
+                break;
+        }
+    }
 }
