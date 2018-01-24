8a6c83ec28aebb03b429aa4a7314efce715a7ff9
ZhangJieBo <530092772@qq.com>
2018-01-24 10:56:36 +0800

    修复7.0apk安装报错

--- app/src/main/java/com/sxmoc/bq/util/UpgradeUtils.java
+++ app/src/main/java/com/sxmoc/bq/util/UpgradeUtils.java
@@ -92,21 +92,6 @@ public class UpgradeUtils extends Activity {
         });
     }
 
-    public static void checkUpgradeIsAble(Context context, String url) {
-        mContext = context;
-        ApiClient.post(context, getOkObject(url), new ApiClient.CallBack() {
-            @Override
-            public void onSuccess(String s) {
-                checkUpgradeIsAble(s);
-            }
-
-            @Override
-            public void onError() {
-
-            }
-        });
-    }
-
     private static void checkUpDialog(String json) {
         upgrade = GsonUtils.parseJSON(json, Upgrade.class);
         int currVersion = VersionUtils.getCurrVersion(mContext);
@@ -389,17 +374,26 @@ public class UpgradeUtils extends Activity {
 //        intent.setDataAndType(Uri.fromFile(file),
 //                "application/vnd.android.package-archive");
 
+
         Intent intent = new Intent(Intent.ACTION_VIEW);
-//判断是否是AndroidN以及更高的版本
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(mContext, BuildConfig.APPLICATION_ID + ".fileProvider", file);
-            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
-        } else {
-            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        //版本在7.0以上是不能直接通过uri访问的
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+            // 由于没有在Activity环境下启动Activity,设置下面的标签
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
+            Uri apkUri = FileProvider.getUriForFile(mContext, BuildConfig.APPLICATION_ID+".myprovider", file);
+            //添加这一句表示对目标应用临时授权该Uri所代表的文件
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.fromFile(file),
+                    "application/vnd.android.package-archive");
         }
         mContext.startActivity(intent);
+
+
+
+
         mNotifiMgr.cancel(12345);
     }
 
